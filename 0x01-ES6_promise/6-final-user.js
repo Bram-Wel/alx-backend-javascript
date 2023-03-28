62a8b0e2cc067cfaaeaaee5a83597279e3c57714
Bram-Wel <welbram@gmail.com>
2023-03-28 14:19:34 +0300

    Use reason & msg from Error Object

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,5 +3,6 @@ import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const value = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
+  value[1].reason = `${value[1].reason.name}: ${value[1].reason.message}`;
   return value;
 }
