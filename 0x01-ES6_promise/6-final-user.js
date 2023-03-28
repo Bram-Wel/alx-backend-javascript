c2e2ad4af3013086a8da0724d75694b7df32a2f9
Bram-Wel <welbram@gmail.com>
2023-03-28 14:53:00 +0300

    Rename reason property to value

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,8 @@ import uploadPhoto from './5-photo-reject';
 export default async function handleProfileSignup(firstName, lastName, fileName) {
   const value = await Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)]);
   value[1].reason = `${value[1].reason.name}: ${value[1].reason.message}`;
+  Object.defineProperty(value[1], 'value',
+    Object.getOwnPropertyDescriptor(value[1], 'reason'));
+  delete value[1].reason;
   return value;
 }
