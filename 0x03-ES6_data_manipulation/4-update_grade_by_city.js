285e019be96e5f3f97765d757828bfed32b620ce
Bram-Wel <welbram@gmail.com>
2023-04-07 14:55:48 +0300

    Typed Arrays

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,8 +1,7 @@
 import getStudentsByCity from "./2-get_students_by_loc"; // eslint-disable-line quotes
 
 export default function updateStudentGradeByCity(students, city, newGrades) {
-  const studentsByCity = getStudentsByCity(students, city);
-  return studentsByCity.map((student) => {
+  return getStudentsByCity(students, city).map((student) => {
     /* eslint no-param-reassign: ["error", { "props": false }] */
     const temp = newGrades.filter((item) => item.studentId === student.id).pop();
     student.grade = temp === undefined ? 'N/A' : temp.grade;
---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,16 @@
+export default function createInt8TypedArray(length, position, value) {
+  /* Array.prototype.forEach.call(arguments, // eslint-disable-line prefer-rest-params
+    (arg) => { throw new TypeError(`${Object.keys(arg)} must be a number`); }); */
+  const buffer = new ArrayBuffer(length);
+  const dataview = new DataView(buffer);
+  try {
+    dataview.setInt8(position, value);
+  } catch (err) {
+    if (err instanceof RangeError) {
+      console.log('Position outside range');
+    } else {
+      throw err;
+    }
+  }
+  return dataview;
+}
