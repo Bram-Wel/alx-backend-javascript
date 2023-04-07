0020ef3d8b9650e41f731c710c694edfc979ad60
Bram-Wel <welbram@gmail.com>
2023-04-07 21:45:03 +0300

    Add check for non-string starts with value

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,7 @@
 export default function cleanSet(set, startString) {
   let str = '';
   set.forEach((element) => {
-    if (startString && element && element.startsWith(startString)) {
+    if (startString && typeof startString === 'string' && element && element.startsWith(startString)) {
       str = str.concat('-', element.slice(startString.length));
       // console.log(`DEBUG: ${element}`);
     }
