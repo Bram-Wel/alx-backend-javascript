0a40c56c72c2ea891e99be1aaf2138aa8e1c7891
Bram-Wel <welbram@gmail.com>
2023-04-07 21:32:29 +0300

    Iterating sets & string manipulation

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+  let str = '';
+  set.forEach((element) => {
+    if (startString && element.startsWith(startString)) {
+      str = str.concat('-', element.slice(startString.length));
+      // console.log(`DEBUG: ${element}`);
+    }
+  });
+  return str.slice(1);
+}
