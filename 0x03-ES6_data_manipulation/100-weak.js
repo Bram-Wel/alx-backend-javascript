f2e221164800aff2ba469aecc5a755bcaf63b438
Bram-Wel <welbram@gmail.com>
2023-04-08 13:45:12 +0300

    Change count check to greater/equal to 4

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,7 +2,7 @@ export const weakMap = new WeakMap();
 
 export function queryAPI(endpoint) {
   let no = weakMap.get(endpoint);
-  if (no > 4) {
+  if (no >= 4) {
     throw new Error('Endpoint load is high');
   } else if (no) {
     weakMap.set(endpoint, ++no); // eslint-disable-line no-plusplus
